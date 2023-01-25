20a5aa17ddd6400d9a816a0a5b5dfdcec4b34002
Jiali Zhang <zhangjiali86@gmail.com>
2023-01-25 15:07:36 +0100

    feat: add task without listing them

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,41 @@
+import {FC, ChangeEvent, useState} from 'react';
 import './App.css';
+import { ITask } from './Interfaces';
 
-function App() {
+const App: FC = () => {
+
+const [task, setTask] = useState<string>("");
+const [deadline, setDeadline] = useState<number>(0);
+const [todoList, setTodoList] = useState<ITask[]>([]);
+
+const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+  if(event.target.name==="Task"){
+    setTask(event.target.value)
+  } else {
+    setDeadline(Number(event.target.value))
+  };
+}
+// (event: ChangeEvent<HTMLInputElement>): void ; event.target.name
+
+const addTask = (): void =>{
+  const newTask = {taskName: task, deadline: deadline };
+  setTodoList([...todoList, newTask]);
+  console.log(todoList);
+}
+// why it doesn't log first item.
+
   return (
     <div className="App">
-      <p>hi</p>
+      <div>
+        <div>
+        <input type="text" placeholder="Task..." name="Task" onChange={handleChange}/>
+        <input type="number" placeholder="Deadline (in Days)..." name="Deadline" onChange={handleChange}/>
+        </div>
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <div>
+        todolist
+      </div>
     </div>
   );
 }
