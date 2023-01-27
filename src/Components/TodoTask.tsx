b4113053f39773bcf37e5e2f9e8d1b9514d72a82
Jiali Zhang <zhangjiali86@gmail.com>
2023-01-27 22:23:36 +0100

    list is done

--- src/Components/TodoTask.tsx
+++ src/Components/TodoTask.tsx
@@ -1,26 +1,30 @@
 import { ITask } from '../Interfaces';
 
-interface Props{
+interface Props {
     task: ITask;
     deleteTask(id: number): void;
 }
 //  deleteTask(id: number): void;
 
 
-export const TodoTask = ({task, deleteTask}: Props) => {
-  return (
-<div className="overflow-x-auto">
-  <table className="table w-full">
-      <tbody>
-      <th>{task.taskName} </th>
-        <th>{task.deadline}</th>
+export const TodoTask = ({ task, deleteTask }: Props) => {
+    return (
+        <div className="flex justify-between items-end my-5">
+            <div>
+                <span>{task.taskName} </span>
+            </div>
 
-      <button className="btn btn-outline btn-warning" onClick={()=>deleteTask(task.id)}>X</button>
-      </tbody>
+            <div className="flex gap-5 items-end">
+                <div>
+                    <span>{task.deadline}</span>
+                </div>
+                <div>
+                    <button className="btn btn-sm btn-outline btn-warning" onClick={() => deleteTask(task.id)}>X</button>
+                </div>
+            </div>
 
-      </table>
-</div>
-  )
+        </div>
+    )
 }
 
 
